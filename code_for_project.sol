be04489389300b00cd9ddb8095474748bc176c06
arfanbinparvez <123824765+arfanbinparvez@users.noreply.github.com>
2024-08-01 22:11:53 +0530

    Update code_for_project.sol

--- code_for_project.sol
+++ code_for_project.sol
@@ -6,40 +6,39 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract DegenToken is ERC20, Ownable, ERC20Burnable {
-    uint256 public constant REDEMPTION_RATE = 100;
+    
 
-    struct ItemOwnership {           //items owned are mapped to the id or name of the item 
-        uint256 quantity;
-    }
+    
+    mapping(address => mapping(uint256 => uint256)) public itemsOwned; // Mapping of user to item id to quantity
 
-    mapping(address => mapping(uint256 => ItemOwnership)) public itemsOwned;
+    
+    mapping(uint256 => string) public itemNames;  // Mapping of item id to item name
 
-    struct GameItem {
-        string name;
-        uint256 cost; 
-        uint256 id; 
-    }
+    
+    mapping(uint256 => uint256) public itemCosts;  // Mapping of item id to item cost
 
-    GameItem[] public gameItems;
-    uint256 public itemIdCounter; 
+    uint256 public itemIdCounter;
 
     constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {
-        _mint(msg.sender, 0);
-        itemIdCounter = 0; 
+        itemIdCounter = 0;
+
+        // Add default game items
+        addGameItem("Ammo", 10);
+        addGameItem("Gun", 50);
+        addGameItem("Health", 15);
     }
 
     function redeemTokens(uint256 itemId, uint256 quantity) public {
         require(itemId < itemIdCounter, "Invalid item ID");
-        GameItem memory item = gameItems[itemId];
-        uint256 cost = item.cost * quantity;
+        uint256 cost = itemCosts[itemId] * quantity;
         require(balanceOf(msg.sender) >= cost, "Not enough tokens to redeem for an item");
 
-        itemsOwned[msg.sender][itemId].quantity += quantity;
+        itemsOwned[msg.sender][itemId] += quantity;
         _burn(msg.sender, cost);
     }
 
-    function checkTokensOwned(address user, uint256 itemId) public view returns (uint256) {
-        return itemsOwned[user][itemId].quantity;
+    function checkTokensOwned(address user) public view returns (uint256) {
+        return balanceOf(user);
     }
 
     function mintTokens(address to, uint256 amount) public onlyOwner {
@@ -53,12 +52,26 @@ contract DegenToken is ERC20, Ownable, ERC20Burnable {
     }
 
     function addGameItem(string memory name, uint256 cost) public onlyOwner {
-        GameItem memory item = GameItem(name, cost, itemIdCounter);
-        gameItems.push(item);
-        itemIdCounter++; 
+        itemNames[itemIdCounter] = name;
+        itemCosts[itemIdCounter] = cost;
+        itemIdCounter++;
     }
 
-    function getGameItems() public view returns (GameItem[] memory) {
-        return gameItems;
+    function getGameItem(uint256 itemId) public view returns (string memory, uint256) {
+        return (itemNames[itemId], itemCosts[itemId]);
     }
+
+function getInventory() public view returns (uint256[] memory, string[] memory, uint256[] memory) {
+    uint256[] memory itemIdsArray = new uint256[](itemIdCounter);
+    string[] memory itemNamesArray = new string[](itemIdCounter);
+    uint256[] memory itemCostsArray = new uint256[](itemIdCounter);
+
+    for (uint256 i = 0; i < itemIdCounter; i++) {
+        itemIdsArray[i] = i;
+        itemNamesArray[i] = itemNames[i];
+        itemCostsArray[i] = itemCosts[i];
+    }
+
+    return (itemIdsArray, itemNamesArray, itemCostsArray);
+}
 }
